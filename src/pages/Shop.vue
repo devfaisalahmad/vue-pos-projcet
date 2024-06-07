227690d60aa5c1442af7d4100c56419502b5a9a8
Faisal Ahmad <devfaisal445@gmail.com>
2024-06-08 02:01:11 +0600

    category design change and keyfram added

--- src/pages/Shop.vue
+++ src/pages/Shop.vue
@@ -7,10 +7,10 @@ import WhatsAppIcon from "../components/icons/WhatsAppIcon.vue";
 
 <template>
   <div class="w-full bg-center bg-cover bg-[url('/image/cate.png')] relative">
-    <div class="w-full flex min-h-screen flex-col justify-between">
+    <div class="w-full flex min-h-screen flex-col">
       <!-- navbar -->
       <nav class="w-full z-40 py-6 px-2">
-        <div class="xl:container mx-auto">
+        <div class="container mx-auto">
           <div class="w-full flex items-center gap-3 justify-between">
             <div>
               <RouterLink
@@ -45,10 +45,11 @@ import WhatsAppIcon from "../components/icons/WhatsAppIcon.vue";
           </div>
         </div>
       </nav>
-
-      <!-- drowar -->
+    </div>
+    <!-- drowar -->
+    <div class="fixed custom animate-bottom">
       <div
-        class="w-full bg-[#f5f5f5] min-h-[330px] rounded-tl-[40px] rounded-tr-[40px] z-30 border-t border-[#f0f0f0] py-4 fixed bottom-0 left-0 animate-bottom"
+        class="bg-[#f5f5f5] min-h-[330px] rounded-[14px] border-[#f0f0f0] py-4 container"
       >
         <div class="container">
           <div class="w-full flex flex-col gap-3">
@@ -73,10 +74,11 @@ import WhatsAppIcon from "../components/icons/WhatsAppIcon.vue";
             </div>
 
             <div
-              class="grid lg:grid-cols-5 md:grid-cols-3 sm:grid-cols-2 grid-cols-2 md:gap-5 sm:gap-4 gap-3 sm:w-[80%] w-full mx-auto"
+              class="grid lg:grid-cols-5 md:grid-cols-3 sm:grid-cols-2 grid-cols-2 md:gap-5 sm:gap-4 gap-3 w-full"
             >
+              <!-- single category -->
               <div
-                class="sm:h-[170px] h-[130px] shadow-lg rounded-xl bg-center bg-cover bg-[url('/image/starting.webp')] relative flex justify-center items-end px-2 py-4"
+                class="w-full h-[200px] shadow-lg rounded-xl bg-center bg-cover bg-[url('/image/starting.webp')] relative flex justify-center items-end px-2 py-4"
               >
                 <span
                   class="sm:text-2xl text-xl font-medium capitalize text-[white] text-shadow z-20"
@@ -85,59 +87,7 @@ import WhatsAppIcon from "../components/icons/WhatsAppIcon.vue";
                 </span>
 
                 <div
-                  class="absolute shadow-lg rounded-xl z-10 bottom-0 gradient-color left-0 w-full h-[170px]"
-                ></div>
-              </div>
-              <div
-                class="sm:h-[170px] h-[130px] shadow-lg rounded-xl bg-center bg-cover bg-[url('/image/react.jpg')] relative flex justify-center items-end px-2 py-4"
-              >
-                <span
-                  class="sm:text-2xl text-xl font-medium capitalize text-[white] text-shadow z-20"
-                >
-                  ReactJS
-                </span>
-
-                <div
-                  class="absolute shadow-lg rounded-xl z-10 bottom-0 gradient-color left-0 w-full h-[170px]"
-                ></div>
-              </div>
-              <div
-                class="sm:h-[170px] h-[130px] shadow-lg rounded-xl bg-center bg-cover bg-[url('/image/start-page.webp')] relative flex justify-center items-end px-2 py-4"
-              >
-                <span
-                  class="sm:text-2xl text-xl font-medium capitalize text-[white] text-shadow z-20"
-                >
-                  Organic
-                </span>
-
-                <div
-                  class="absolute shadow-lg rounded-xl z-10 bottom-0 gradient-color left-0 w-full h-[170px]"
-                ></div>
-              </div>
-              <div
-                class="sm:h-[170px] h-[130px] shadow-lg rounded-xl bg-center bg-cover bg-[url('/image/pic-1.png')] relative flex justify-center items-end px-2 py-4"
-              >
-                <span
-                  class="sm:text-2xl text-xl font-medium capitalize text-[white] text-shadow z-20"
-                >
-                  Start Now
-                </span>
-
-                <div
-                  class="absolute shadow-lg rounded-xl z-10 bottom-0 gradient-color left-0 w-full h-[170px]"
-                ></div>
-              </div>
-              <div
-                class="sm:h-[170px] h-[130px] shadow-lg rounded-xl bg-center bg-cover bg-[url('/image/pic-1.png')] relative flex justify-center items-end px-2 py-4"
-              >
-                <span
-                  class="sm:text-2xl text-xl font-medium capitalize text-[white] text-shadow z-20"
-                >
-                  All Category
-                </span>
-
-                <div
-                  class="absolute shadow-lg rounded-xl z-10 bottom-0 gradient-color left-0 w-full h-[170px]"
+                  class="absolute shadow-lg rounded-xl z-10 bottom-0 gradient-color left-0 w-full h-full"
                 ></div>
               </div>
             </div>
@@ -153,8 +103,11 @@ import WhatsAppIcon from "../components/icons/WhatsAppIcon.vue";
   0% {
     bottom: -100%;
   }
+  50% {
+    bottom: -135px;
+  }
   100% {
-    bottom: 0;
+    bottom: -145px;
   }
 }
 
